68c4a9f95b07e6a2380f0f8f76d86a27dc210f9f
Laura Hall <podinjshmi1993@gmail.com>
2023-07-06 11:18:43 +0100

    chnged useState name and fixed the big

--- src/components/FizzBuzzBody.tsx
+++ src/components/FizzBuzzBody.tsx
@@ -1,48 +1,39 @@
-
-//pseudo code
-/* 1. welcome to Fizz buzz game text
-   2. import state
-
-    display the numbers/ text 
-    button for +1 
-
-*/
 import {useState} from "react";
 
 
 function FizzBuzzBody(): JSX.Element {
- const [counterValueFromCurrentRender, queueRenderWithNewCounterValue] = useState(0);
- const [storedValueFromCurrentRender, queueRenderWithNewStoredValue] = useState<(number | string)[]>([]);
+ const [countValue, setCountValue] = useState(0);
+ const [storeCountValue, setStoreCountValue] = useState<(number | string)[]>([]);
 
  const handleAddOneToLine = () => {
-    queueRenderWithNewCounterValue(counterValueFromCurrentRender + 1);
+    setCountValue(countValue + 1);
     handleStoredCurrentCount();
   };
   
 
  const handleStoredCurrentCount = () => {
-    if(counterValueFromCurrentRender % 5 === 0 && counterValueFromCurrentRender % 3 === 0 ){
-        queueRenderWithNewStoredValue([
-            ...storedValueFromCurrentRender,
+    if(countValue % 5 === 0 && countValue % 3 === 0 && countValue > 0 ){
+        setStoreCountValue([
+            ...storeCountValue,
             " Fizz Buzz"]);
-    } else if(counterValueFromCurrentRender % 3 === 0){
-        queueRenderWithNewStoredValue([
-            ...storedValueFromCurrentRender,
+    } else if(countValue % 3 === 0 && countValue > 0){
+        setStoreCountValue([
+            ...storeCountValue,
             "Fizz"]);
-    } else if (counterValueFromCurrentRender % 5 === 0){
-        queueRenderWithNewStoredValue([
-            ...storedValueFromCurrentRender,
+    } else if (countValue % 5 === 0 && countValue > 0){
+        setStoreCountValue([
+            ...storeCountValue,
             "Buzz"]);
     } else {
-        queueRenderWithNewStoredValue([
-        ...storedValueFromCurrentRender,
-        counterValueFromCurrentRender])
+        setStoreCountValue([
+        ...storeCountValue,
+        countValue])
     }
  }
 
  const handleStartNewGame = () => {
-    queueRenderWithNewStoredValue([0]);
-    queueRenderWithNewCounterValue(0);
+    setStoreCountValue([]);
+    setCountValue(0);
  }
 
 
@@ -51,7 +42,7 @@ function FizzBuzzBody(): JSX.Element {
         <section>
             <h3>Lets Play the FizzBuzz Game!</h3>
             <p>Press the Button to find Fiz, Buzz or FizzBuzz...</p>
-            <p>Your Number Line :{storedValueFromCurrentRender.join(', ')}</p>
+            <p>Your Number Line :{storeCountValue.join(', ')}</p>
             <button onClick={handleAddOneToLine}>Add a Number</button>
             <button onClick={handleStartNewGame}>Start New Game</button>
         </section>
